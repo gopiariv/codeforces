cfc05d30ca671490934e37bcc10f4deea5ddb6e8
Gopi Arivalagan <gopiariv@gmail.com>
2014-11-22 03:54:22 +0530

    488A.py

--- 488A.py
+++ 488A.py
@@ -1,9 +1,11 @@
 def main():
 	n = int(input())
-	for i in range(17):
+	for i in range(1,17):
 		if '8' in str(a+i):
 			print(i)
 			break
 
 if __name__ == '__main__':
 	main()
+
+"""Critical case is when a = -8 . So i's range should be greater than 16"""
